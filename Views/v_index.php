0dbb37e511f722dff622d43ad4ae6f170cec14a8
Leonona <jalu717@gmail.com>
2018-04-24 05:23:16 +0700

    pppp

--- Views/v_index.php
+++ Views/v_index.php
@@ -89,7 +89,7 @@
               <div class="card mb-3 box-shadow">
                 <img class="card-img-top" src="assets/gambar/g1.jpg">
                 <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text" >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                   <div class="d-flex justify-content-between align-items-center">
                     
                     <small class="text-muted " >9 mins</small>
